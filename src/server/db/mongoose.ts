248d844e2d652a601d57f2e9cc0cab6c0a2887ed
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-06-10 17:00:55 -0700

    Server: Updated mongoDB setup with database options from common folder

--- src/server/db/mongoose.ts
+++ src/server/db/mongoose.ts
@@ -1,12 +1,8 @@
 import mongoose from 'mongoose';
+import { mongoDbOptions } from '../../common';
 
 if (!process.env.DATABASE_URI) throw new Error('No Database Connection String');
 
 mongoose
-  .connect(process.env.DATABASE_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.DATABASE_URI, mongoDbOptions)
   .then(() => console.log('Connected to DB'));
