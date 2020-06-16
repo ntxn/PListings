193b71b680a776cdd88a79ec2e90841bffc4a93e
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-06-15 17:45:06 -0700

    Updated error messages

--- src/common/error-messages.ts
+++ src/common/error-messages.ts
@@ -1,8 +1,13 @@
 export enum ErrMsg {
   NameRequired = 'Please enter your name',
   EmailRequired = 'Please enter your email address',
-  EmailExisted = 'Email address already exists, please sign in',
+  EmailInvalid = 'Please enter a valid email address',
   PasswordRequired = 'Please enter your password',
   PasswordMinLength = 'Password requirement: minium 8 characters',
+  PasswordConfirmRequired = 'Please confirm your password',
+  PasswordConfirmNotMatch = 'Password confirmation does not match',
   BioMaxLength = 'Your biography should be less than 150 characters',
+  DuplicateKey = 'MongoDB Duplicate Key Error',
+  JwtInvalid = 'Invalid token. Please log in again',
+  JwtExpired = 'Your token has expired. Please log in again',
 }
