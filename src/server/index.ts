acb7af34b7b5e6184b78d227b0c8bd506c10b3c9
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-06-15 17:41:27 -0700

    Server: Added function to handle uncaughtException and unhandledRejection

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,20 @@
 import { app } from './app';
+
+process.on('uncaughtException', (err: Error): void => {
+  console.log('UNCAUGHT EXCEPTION');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 import './db/mongoose';
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`API SERVER listening on port ${port}`));
+const server = app.listen(port, () =>
+  console.log(`API SERVER listening on port ${port}`)
+);
+
+process.on('unhandledRejection', (err: Error): void => {
+  console.log('UNHANDLED REJECTION');
+  console.log(err.name, err.message);
+  server.close(() => process.exit(1)); // 0 for success, 1 is for unhandled rejection);
+});
