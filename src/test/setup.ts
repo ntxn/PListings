e3db34f9d17fb69e73deb9cc10c818e104d96305
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-06-18 17:08:18 -0700

    Server: Setup a global login function for testing and use email mock functions for all tests

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,6 +1,19 @@
-import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
-import { mongoDbOptions } from '../common';
+import request from 'supertest';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import { app } from '../server/app';
+import { mongoDbOptions, ResourceRoutes, Routes } from '../common';
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace NodeJS {
+    interface Global {
+      login(email: string): Promise<string[]>;
+    }
+  }
+}
+
+jest.mock('../server/utils/email');
 
 let mongoServer: MongoMemoryServer;
 
@@ -13,6 +26,7 @@ beforeAll(async () => {
 });
 
 beforeEach(async () => {
+  jest.clearAllMocks();
   // Clear database before each test
   const collections = await mongoose.connection.db.collections();
   for (const collection of collections) await collection.deleteMany({});
@@ -23,3 +37,12 @@ afterAll(async () => {
   await mongoServer.stop();
   await mongoose.connection.close();
 });
+
+global.login = async (email: string) => {
+  const response = await request(app)
+    .post(`${ResourceRoutes.Auth}${Routes.LogIn}`)
+    .send({ email, password: 'password' })
+    .expect(200);
+
+  return response.get('Set-Cookie');
+};
