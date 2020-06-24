290774173018bdca80119699ca1473f055fac2ea
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-06-23 17:37:24 -0700

    Created an enum for item conditions

---
+++ src/common/listing-conditions.ts
@@ -0,0 +1,15 @@
+export enum Conditions {
+  New = 'New',
+  LikeNew = 'Like New',
+  Good = 'Good',
+  Fair = 'Fair',
+  Poor = 'Poor',
+}
+
+export enum ConditionsMeaning {
+  New = 'Has never been used or is in original packaging',
+  LikeNew = 'Used a couple times or removed from original packaging',
+  Good = 'Gently used, shows wear but continues to function',
+  Fair = 'Used, shows wear and functions totally or partially',
+  Poor = 'Heavily used or damaged, does not function anymore',
+}
