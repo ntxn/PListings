b558d31a9414ab31ac5ce1cbfe530fa392e7a948
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-06-30 17:52:33 -0700

    Client: Setup app to use redux and redux thunk

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 
 import './sass/main.scss';
 import { App } from './components/App';
+import { reducers } from './reducers';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+const store = createStore(
+  reducers,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
