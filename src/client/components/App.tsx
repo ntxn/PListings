fc5a3918a3a3b5325d57d3c1e1318c1aa73ad5d6
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-06-30 17:51:13 -0700

    Client: set up basic app with custom browser router, routes to Home and Sign up

--- src/client/components/App.tsx
+++ src/client/components/App.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
 
-const App = () => {
-  return <div>FRONT END</div>;
+import { history } from '../history';
+import { Header } from './Header';
+import { Footer } from './Footer';
+import { Listings } from './Listings';
+import { UserCreate } from './UserCreate';
+
+export const App = (): JSX.Element => {
+  return (
+    <Router history={history}>
+      <div className="wrapper">
+        <Header />
+        <main>
+          <div className="main">
+            <Switch>
+              <Route path="/" exact component={Listings} />
+              <Route path="/users/signup" exact component={UserCreate} />
+            </Switch>
+          </div>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
 };
-
-export { App };
---
+++ src/client/components/Listings.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Listings = (): JSX.Element => {
+  return (
+    <div>
+      <h1>Listingsss</h1>
+      <p>All the listing</p>
+    </div>
+  );
+};
