76759e30a0b9e9370a8b4a6e4ed2f6b2d083a71f
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-06-30 17:49:09 -0700

    Client: created a simple footer with plistings logo and placeholders for app navigation

---
+++ src/client/components/Footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export const Footer = (): JSX.Element => {
+  return (
+    <footer>
+      <div className="footer">
+        <div className="footer__logo">
+          <Link to="/">
+            <img src="/img/logo/logo-white.png" alt="plistings logo in white" />
+          </Link>
+        </div>
+        <div className="footer__navigation">Navigation</div>
+      </div>
+    </footer>
+  );
+};
