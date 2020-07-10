0c224874d9d9a83954a166adc25f3f0677ef529e
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-07-10 15:33:47 -0700

    Client: added interfaces for update profile and password actions, form props and input attributes, and a HTML Event Element with target

--- src/client/utilities/index.ts
+++ src/client/utilities/index.ts
@@ -1,2 +1,4 @@
 export * from './action-types';
 export * from './interfaces';
+export * from './catch-submission-error';
+export * from './form-field-values';
--- src/client/utilities/interfaces.ts
+++ src/client/utilities/interfaces.ts
@@ -1,9 +1,13 @@
+import { Dispatch, Action as ReduxAction } from 'redux';
+import { FormStateMap } from 'redux-form';
+
 import { ActionTypes } from './action-types';
 import { UserDoc } from '../../server/models';
 import { GeoLocation } from '../../common';
-import { FormStateMap } from 'redux-form';
 
-// Actions
+// ACTIONS
+
+// auth
 export interface FetchCurrentUserAction {
   type: ActionTypes.fetchCurrentUser;
   payload: UserDoc | null;
@@ -24,6 +28,17 @@ export interface LogOutAction {
   payload: null;
 }
 
+export interface UpdateProfileAction {
+  type: ActionTypes.updateProfile;
+  payload: UserDoc;
+}
+
+export interface UpdatePasswordAction {
+  type: ActionTypes.updatePassword;
+  payload: UserDoc;
+}
+
+// Location
 export interface GetLocationWithPermissionAction {
   type: ActionTypes.getLocationWithPermission;
   payload: GeoLocation;
@@ -39,6 +54,8 @@ export type Action =
   | SignUpAction
   | LogOutAction
   | LogInAction
+  | UpdateProfileAction
+  | UpdatePasswordAction
   | GetLocationWithPermissionAction
   | GetLocationByIPAction;
 
@@ -47,4 +64,58 @@ export interface StoreState {
   user: UserDoc | null;
   form: FormStateMap;
   location: GeoLocation;
+}
+
+//
+export type FunctionalAction<A extends ReduxAction> = (
+  dispatch: Dispatch<A>,
+  getState?: () => StoreState
+) => Promise<void>;
+
+// FORM
+export interface CustomFormProps {
+  label: string;
+  type: string;
+  placeholder: string;
+  required: boolean;
+}
+
+interface FormFields extends CustomFormProps {
+  name: string;
+}
+
+export interface FormProps<Attrs> {
+  onSubmit(formValues: Attrs): void;
+  formFields: FormFields[];
+}
+
+export interface LogInAttrs {
+  email: string;
+  password: string;
+}
+
+export interface SignUpAttrs {
+  name: string;
+  email: string;
+  password: string;
+  passwordConfirm: string;
+}
+
+export interface UpdatePasswordAttrs {
+  currentPassword: string;
+  password: string;
+  passwordConfirm: string;
+}
+
+export interface UpdateProfileAttrs {
+  name: string;
+  email: string;
+  location: string;
+  photo?: string;
+  bio?: string;
+}
+
+// HTML ELEMENT
+export interface EventWithTarget {
+  target: HTMLInputElement;
 }
