5770bd2f4d0d66670ea4654ed3218ddab117f80d
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-07-13 14:28:26 -0700

    Client: styled and added functionality to settings menu

--- src/client/components/user/AccountSettings.tsx
+++ src/client/components/user/AccountSettings.tsx
@@ -25,7 +25,17 @@ interface DispatchProps {
 
 type AccountSettingsProps = StateProps & DispatchProps;
 
+enum FormIds {
+  profile = 'update-profile-form',
+  password = 'update-password-form',
+}
+
 const _AccountSettings = (props: AccountSettingsProps): JSX.Element => {
+  const dropdownOptions = {
+    [FormIds.profile]: 0,
+    [FormIds.password]: 1,
+  };
+
   const renderUpdatePasswordForm = (): JSX.Element => {
     const onSubmit = (formValues: UpdatePasswordAttrs) =>
       props.updatePassword(formValues);
@@ -33,8 +43,16 @@ const _AccountSettings = (props: AccountSettingsProps): JSX.Element => {
     const formFields = [currentPassword, password, passwordConfirm];
 
     return (
-      <div className="container__form">
-        <UpdatePasswordForm onSubmit={onSubmit} formFields={formFields} />
+      <div className="container__form u-hide" id={FormIds.password}>
+        <h2 className="heading-primary u-margin-bottom-xxsmall">
+          Change password
+        </h2>
+        <hr className="u-divider u-margin-bottom-medium" />
+        <UpdatePasswordForm
+          onSubmit={onSubmit}
+          formFields={formFields}
+          submitBtnText="Update Password"
+        />
       </div>
     );
   };
@@ -52,7 +70,11 @@ const _AccountSettings = (props: AccountSettingsProps): JSX.Element => {
     };
 
     return (
-      <div className="container__form">
+      <div className="container__form" id={FormIds.profile}>
+        <h2 className="heading-primary u-margin-bottom-xxsmall">
+          Public profile
+        </h2>
+        <hr className="u-divider u-margin-bottom-medium" />
         <UpdateProfileForm
           onSubmit={onSubmit}
           initialValues={initialValues}
@@ -63,46 +85,65 @@ const _AccountSettings = (props: AccountSettingsProps): JSX.Element => {
     );
   };
 
+  const showForm = (formId: FormIds): void => {
+    const forms = document.querySelector('.settings__content')!.children;
+    const options = document.querySelector('.settings__dropdown__content')!
+      .children;
+
+    for (let i = 0; i < forms.length; i++) {
+      forms[i].classList.add('u-hide');
+      options[i].classList.remove('settings__dropdown__option__active');
+    }
+
+    const activeForm = document.getElementById(formId);
+    activeForm!.classList.remove('u-hide');
+    options[dropdownOptions[formId]].classList.add(
+      'settings__dropdown__option__active'
+    );
+  };
+
   const renderSettings = (): JSX.Element => {
     return (
       <div className="settings">
         <div className="settings__dropdown">
           <FiMenu className="settings__dropdown__btn" />
           <div className="settings__dropdown__content">
-            <div className="settings__dropdown__option">
-              <MdPersonPin className="settings__dropdown__option__icon" />
-              <span className="settings__dropdown__option__title">Profile</span>
+            <div
+              className="settings__dropdown__option settings__dropdown__option__active"
+              onClick={() => showForm(FormIds.profile)}
+            >
+              <MdPersonPin />
+              <span>Profile</span>
+            </div>
+            <div
+              className="settings__dropdown__option"
+              onClick={() => showForm(FormIds.password)}
+            >
+              <RiLockPasswordLine />
+              <span>Password</span>
             </div>
             <div className="settings__dropdown__option">
-              <RiLockPasswordLine className="settings__dropdown__option__icon" />
-              <span className="settings__dropdown__option__title">
-                Password
-              </span>
-            </div>
-            <div className="settings__dropdown__option">
-              <MdNotifications className="settings__dropdown__option__icon" />
-              <span className="settings__dropdown__option__title">
-                Notifications
-              </span>
+              <MdNotifications />
+              <span>Notifications</span>
             </div>
           </div>
         </div>
         <div className="settings__content">
-          {renderUpdatePasswordForm()}
           {renderUpdateProfileForm()}
+          {renderUpdatePasswordForm()}
         </div>
       </div>
     );
   };
 
   return (
-    <div>
+    <>
       {props.user ? (
         renderSettings()
       ) : (
         <AuthRequired route="Account Settings" />
       )}
-    </div>
+    </>
   );
 };
 
