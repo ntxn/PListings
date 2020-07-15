cee6dc64b79953b433e8754d9552584f06a3965f
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-07-15 12:46:32 -0700

    Client: added action creator for searching location

--- src/client/actions/location.ts
+++ src/client/actions/location.ts
@@ -1,16 +1,21 @@
 import axios from 'axios';
 import { Dispatch } from 'redux';
+import { SubmissionError } from 'redux-form';
 import { ActionTypes } from '../utilities/action-types';
-import { GeoLocation } from '../../common';
+import { GeoLocation, ErrMsg } from '../../common';
 import {
   GetLocationWithPermissionAction,
   GetLocationByIPAction,
+  SearchLocationAction,
+  SearchedLocation,
+  catchSubmissionError,
+  FunctionalAction,
 } from '../utilities';
 
 export const getLocationWithPermission = (): GetLocationWithPermissionAction => {
   const location: GeoLocation = {
     coordinates: [-122.437598, 37.757591], // [longitude, latitude]
-    postal: 94114,
+    zip: 94114,
     city: 'San Francisco',
     state: 'California',
     country: 'United States',
@@ -33,16 +38,39 @@ export const getLocationByIP = () => {
       `https://geolocation-db.com/json/${process.env.GEOLOCATION_DB_KEY}`
     );
 
-    const { longitude, latitude, postal, city, state, country_name } = data;
+    const { longitude, latitude, zip, city, state, country_name } = data;
     dispatch<GetLocationByIPAction>({
       type: ActionTypes.getLocationByIP,
       payload: {
         coordinates: [longitude, latitude], // [longitude, latitude]
-        postal: parseInt(postal),
+        zip: parseInt(zip),
         city,
         state,
         country: country_name,
       },
     });
+  };
+};
+
+export const searchLocation = (searchTerm = '') => {
+  return async (dispatch: Dispatch): Promise<void> => {
+    try {
+      let payload: SearchedLocation[] = [];
+
+      if (searchTerm !== '') {
+        const response = await axios.get(
+          `https://public.opendatasoft.com/api/records/1.0/search/?dataset=us-zip-code-latitude-and-longitude&q=${encodeURI(
+            searchTerm
+          )}`,
+          { headers: { Authorization: process.env.OPEN_DATA_SOFT_KEY } }
+        );
+        payload = response.data.records;
+      }
+
+      dispatch<SearchLocationAction>({
+        type: ActionTypes.searchLocation,
+        payload,
+      });
+    } catch (err) {}
   };
 };
