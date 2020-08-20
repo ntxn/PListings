27f7e14100a29151854db2733c543ee80ff40544
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-08-20 15:27:43 -0700

    Server: added handler for delete a favorite document (unsave a listing)

--- src/server/controllers/favoriteController.ts
+++ src/server/controllers/favoriteController.ts
@@ -31,14 +31,42 @@ class FavoriteController {
       if (listing.owner.id === req.user!.id)
         return next(new BadRequestError(ErrMsg.OwnerCannotFavoriteOwnListing));
 
-      listing.favorites = listing.favorites + 1;
-      await listing.save();
-
       const favorite = Favorite.build({
         user: req.user!.id,
         listing: listing.id,
       });
       await favorite.save();
+
+      listing.favorites = listing.favorites + 1;
+      await listing.save();
+
+      res.status(200).json({ status: RequestStatus.Success, data: listing });
+    })(req, res, next);
+  }
+
+  /**
+   * Delete a favorite document - i.e. the current user want to unsave the listing.
+   * The current user can only remove the saved listing that they save before.
+   * Decrements number of listing's favorites
+   *
+   * TODO: write unit test
+   */
+  @use(authenticationChecker)
+  @DELETE(Routes.Favorites)
+  unsaveListing(req: CustomRequest, res: Response, next: NextFunction): void {
+    catchAsync(async (req: CustomRequest, res, next) => {
+      const listing = await Listing.findById(req.body.listingId);
+      if (!listing) return next(new NotFoundError(ErrMsg.NoDocWithId));
+
+      const favorite = await Favorite.findOneAndDelete({
+        user: req.user!.id,
+        listing: req.body.listingId,
+      });
+
+      if (favorite) {
+        listing.favorites = listing.favorites - 1;
+        await listing.save();
+      }
 
       res.status(200).json({ status: RequestStatus.Success, data: listing });
     })(req, res, next);
