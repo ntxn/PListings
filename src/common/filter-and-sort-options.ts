b74e01beea3977506b4983f525e4137707e2e261
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-08-25 14:28:13 -0700

    Added Posted within options

--- src/common/filter-and-sort-options.ts
+++ src/common/filter-and-sort-options.ts
@@ -6,28 +6,21 @@ export const SortBy = {
 };
 
 export const SortOptions = {
-  [SortBy.NewestFirst]: 'createdAt',
+  [SortBy.NewestFirst]: '-createdAt',
   [SortBy.ClosestFirst]: 'location',
   [SortBy.PriceLowToHigh]: 'price',
   [SortBy.PriceHighToLow]: '-price',
 };
 
-export enum PostedWithin {
-  AllListings = 'All listings',
-  Last24h = 'Last 24h',
-  Last7d = 'Last 7 days',
-  Last30d = 'Last 30 days',
-}
+export const PostedWithin = {
+  AllListings: 'All listings',
+  Last24h: 'Last 24h',
+  Last7d: 'Last 7 days',
+  Last30d: 'Last 30 days',
+};
 
-// export const PostedWithinOption = (date: Date, option: PostedWithin): string | undefined => {
-//   let newDate: Date;
-//   const yearMonthDate
-
-//   switch (option) {
-//     case PostedWithin.AllListings:
-//       return undefined;
-//     case PostedWithin.Last24h:
-//       newDate = date.setHours(date.getHours() + 24);
-
-//   }
-// };
+export const PostedWithinOption = {
+  [PostedWithin.Last24h]: 1,
+  [PostedWithin.Last7d]: 7,
+  [PostedWithin.Last30d]: 30,
+};
