441f2614c28833a156bb7fb1acf604625558a1ec
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-08-26 19:52:01 -0700

    Client: display ListingCard based on all the queried listings, and prepare the current center location

--- src/client/components/listings/viewAll.tsx
+++ src/client/components/listings/viewAll.tsx
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 
 import { fetchListings } from '../../actions';
+import { ListingCard } from './ListingCard';
 import {
   StoreState,
   FilterAttrs,
   processFiltersToQueryString,
+  calcDistanceBetweenTwoPoints,
 } from '../../utilities';
 import { ListingDoc } from '../../../server/models';
 
@@ -21,6 +23,7 @@ type AllListingsProps = StateProps &
   DispatchProps & { location: { search: string } };
 
 const AllListings = (props: AllListingsProps): JSX.Element => {
+  const [center, setCenter] = useState([0, 0]);
   const [defaultFilterTimer, setDefaultFilterTimer] = useState(
     setTimeout(() => {}, 10000)
   );
@@ -28,11 +31,26 @@ const AllListings = (props: AllListingsProps): JSX.Element => {
   useEffect(() => {
     const { search } = props.location;
 
-    if (search) props.fetchListings(search.substring(1));
-    else {
+    if (search) {
+      const queries = new URLSearchParams(search);
+      setCenter(
+        queries
+          .get('location')!
+          .split(',')
+          .map(value => parseFloat(value))
+      );
+      props.fetchListings(search.substring(1));
+    } else {
       clearTimeout(defaultFilterTimer);
       setDefaultFilterTimer(
         setTimeout(() => {
+          const {
+            coordinates,
+            latitude,
+            longitude,
+          } = props.defaultFilters.location;
+          setCenter(coordinates ? coordinates : [longitude!, latitude!]);
+
           const queryStr = processFiltersToQueryString(props.defaultFilters);
           props.fetchListings(queryStr);
         }, 500)
@@ -42,9 +60,21 @@ const AllListings = (props: AllListingsProps): JSX.Element => {
 
   return (
     <div>
-      {props.listings.map(listing => (
-        <div key={listing.id}>{listing.title}</div>
-      ))}
+      {props.listings.map(listing => {
+        const [lng2, lat2] = listing.location.coordinates;
+        return (
+          <ListingCard
+            key={listing.id}
+            listing={listing}
+            distanceDiff={calcDistanceBetweenTwoPoints(
+              center[1],
+              center[0],
+              lat2,
+              lng2
+            )}
+          />
+        );
+      })}
     </div>
   );
 };
