a682c50426a95526f67f438c8e327b5f1387a79b
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-09-01 20:25:23 -0700

    Client: added a save listing btn to ListingCard

--- src/client/components/listings/ListingCard.tsx
+++ src/client/components/listings/ListingCard.tsx
@@ -1,17 +1,32 @@
 import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { FaHeart } from 'react-icons/fa';
 
-import { ListingDoc } from '../../../server/models';
+import { ListingDoc, UserDoc } from '../../../server/models';
 import { ImageSlider } from '../ImageSlider';
+import { promptUserToLogInToSaveListing } from '../Modal';
+import { StoreState } from '../../utilities';
+import { unsaveListing, saveListing } from '../../actions';
 
 interface ListingCardProps {
+  // from Parent component
   listing: ListingDoc;
   distanceDiff: number;
+
+  // from StoreState
+  user: UserDoc | null;
+  savedListings: Record<string, string>;
+
+  // From dispatch
+  saveListing(listingId: string): void;
+  unsaveListing(listingId: string): void;
 }
 
-export const ListingCard = (props: ListingCardProps): JSX.Element => {
+const _ListingCard = (props: ListingCardProps): JSX.Element => {
   const listingPage = `/listings/${props.listing.id}`;
   const [showImageSlider, setShowImageSlider] = useState(false);
+  const [showLogInModal, setShowLogInModal] = useState(false);
 
   useEffect(() => {
     const imageSliderTimeout = setTimeout(() => setShowImageSlider(true), 500);
@@ -28,6 +43,7 @@ export const ListingCard = (props: ListingCardProps): JSX.Element => {
             pagination
             linkTo={listingPage}
             backgroundSize="cover"
+            bordered
           />
         )}
       </div>
@@ -46,7 +62,40 @@ export const ListingCard = (props: ListingCardProps): JSX.Element => {
           ${props.listing.price}
         </p>
       </div>
-      <div className="listing-card__save-btn"></div>
+      {(!props.user || props.user.id !== props.listing.owner.id) && (
+        <div
+          className={`listing-card__save-btn ${
+            props.savedListings[props.listing.id]
+              ? 'listing-card__save-btn--saved'
+              : ''
+          }`}
+          onClick={() => {
+            if (props.user) {
+              if (props.savedListings[props.listing.id])
+                props.unsaveListing(props.listing.id);
+              else props.saveListing(props.listing.id);
+            } else setShowLogInModal(true);
+          }}
+        >
+          <FaHeart
+            title={`${
+              props.savedListings[props.listing.id] ? 'Unsave' : 'Save'
+            } listing`}
+          />
+        </div>
+      )}
+      {promptUserToLogInToSaveListing(showLogInModal, () =>
+        setShowLogInModal(false)
+      )}
     </div>
   );
 };
+
+const mapStateToProps = (state: StoreState) => {
+  return { user: state.user, savedListings: state.savedListings };
+};
+
+export const ListingCard = connect(mapStateToProps, {
+  saveListing,
+  unsaveListing,
+})(_ListingCard);
