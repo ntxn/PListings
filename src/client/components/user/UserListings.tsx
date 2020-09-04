abb372fbdaf6d9c5e533fb5c6bdbf96c5cddb2a3
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-09-04 14:07:05 -0700

    Client: UserListings displays different listings when clicking on different nav bar options

--- src/client/components/user/UserListings.tsx
+++ src/client/components/user/UserListings.tsx
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
+import axios from 'axios';
 
 import { AuthRequired } from '../AuthRequired';
 import { StoreState } from '../../utilities';
-import { UserDoc } from '../../../server/models';
+import { Loader } from '../Modal';
+import { ListingCardPublic, ListingCardPrivate } from '../ListingCard';
+import {
+  MyListingsTypes,
+  DEFAULT_MY_LISTINGS,
+  UserDoc,
+  ApiRoutes,
+} from '../../../common';
 
 interface UserListingsProps {
   user: UserDoc | null;
 }
 
 const _UserListings = (props: UserListingsProps): JSX.Element => {
+  useEffect(() => {
+    setShowLoader(true);
+    const fetchListings = async () => {
+      const { data } = await axios.get(ApiRoutes.MyListings);
+      if (data) setListings(data.data);
+      setShowLoader(false);
+    };
+
+    fetchListings();
+  }, []);
+
+  const [activeListingType, setActiveListingType] = useState(
+    MyListingsTypes.Selling
+  );
+  const [listings, setListings] = useState(DEFAULT_MY_LISTINGS);
+  const [showLoader, setShowLoader] = useState(false);
+
   const onClickNavItemWithId = (id: string): void => {
     const currentlyActive = document.querySelector(
       '.user-listings__nav__item--active'
@@ -20,6 +45,23 @@ const _UserListings = (props: UserListingsProps): JSX.Element => {
     nextActive!.classList.add('user-listings__nav__item--active');
   };
 
+  const renderListings = (): JSX.Element => {
+    return (
+      <>
+        {listings[activeListingType].map(listing => {
+          return (
+            <ListingCardPrivate
+              key={listing.id}
+              listing={listing}
+              btnText="hello"
+              btnAction={() => console.log(listing.id)}
+            />
+          );
+        })}
+      </>
+    );
+  };
+
   const renderContent = (): JSX.Element => {
     return (
       <div className="user-listings">
@@ -28,39 +70,56 @@ const _UserListings = (props: UserListingsProps): JSX.Element => {
           <span
             className="user-listings__nav__item user-listings__nav__item--active"
             id="user-listings--selling"
-            onClick={() => onClickNavItemWithId('user-listings--selling')}
+            onClick={() => {
+              onClickNavItemWithId('user-listings--selling');
+              setActiveListingType(MyListingsTypes.Selling);
+            }}
           >
             Selling
           </span>
           <span
             className="user-listings__nav__item"
             id="user-listings--saved"
-            onClick={() => onClickNavItemWithId('user-listings--saved')}
+            onClick={() => {
+              onClickNavItemWithId('user-listings--saved');
+              setActiveListingType(MyListingsTypes.Saved);
+            }}
           >
             Saved
           </span>
           <span
             className="user-listings__nav__item"
             id="user-listings--expired"
-            onClick={() => onClickNavItemWithId('user-listings--expired')}
+            onClick={() => {
+              onClickNavItemWithId('user-listings--expired');
+              setActiveListingType(MyListingsTypes.Expired);
+            }}
           >
             Expired
           </span>
           <span
             className="user-listings__nav__item"
             id="user-listings--sold"
-            onClick={() => onClickNavItemWithId('user-listings--sold')}
+            onClick={() => {
+              onClickNavItemWithId('user-listings--sold');
+              setActiveListingType(MyListingsTypes.Sold);
+            }}
           >
             Sold
           </span>
         </div>
-        <div className="user-listings__content listings"></div>
+        <div className="user-listings__content listings">
+          {renderListings()}
+        </div>
       </div>
     );
   };
 
   return (
-    <>{props.user ? renderContent() : <AuthRequired route="Your listings" />}</>
+    <>
+      {props.user ? renderContent() : <AuthRequired route="Your listings" />}
+      {showLoader && <Loader />}
+    </>
   );
 };
 
