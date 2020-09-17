160a15e1e862ff35282cd8608ddd657dec58f8dc
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-09-17 12:01:34 -0700

    Client: created another get time ago method to mix between time ago and date time depending how far ago the provided date was

--- src/client/utilities/get-date-time.ts
+++ src/client/utilities/get-date-time.ts
@@ -5,8 +5,11 @@
 export const getTimeAgo = (date: Date): string => {
   const time = new Date(date).getTime();
   const now = new Date().getTime();
-  let diff = Math.round((now - time) / (1000 * 60)); // minutes
-  if (diff < 60) return `${diff === 0 ? 1 : diff}m ago`;
+  let diff = Math.round((now - time) / 1000); // seconds
+  if (diff < 60) return diff < 10 ? 'Just now' : `${diff}s ago`;
+
+  diff = Math.round(diff / 60); // minutes
+  if (diff < 60) return `${diff}m ago`;
 
   diff = Math.round(diff / 60); // hours
   if (diff < 24) return `${diff}h ago`;
@@ -30,6 +33,34 @@ export const getDateTimeStr = (d: Date): string => {
 
   if (diff < 24) options = { hour: 'numeric', minute: 'numeric' };
   else if (date.getFullYear() === now.getFullYear())
+    options = { month: 'short', day: 'numeric' };
+  else options = { year: 'numeric', month: 'numeric', day: 'numeric' };
+
+  return date.toLocaleString('en-US', options);
+};
+
+/**
+ * Display time ago (ex: 10s ago, 2m ago, 3h ago, 5 days ago) when it's less than a week.
+ * After that display MMM YY if it's still in the same year, or MM/DD/YY when it's already in the past years.
+ */
+export const getTimeAgoAndDateStr = (d: Date): string => {
+  const date = new Date(d);
+  const now = new Date();
+  let diff = Math.round((now.getTime() - date.getTime()) / 1000); // seconds
+  if (diff < 60) return diff < 10 ? 'Just now' : `${diff}s ago`;
+
+  diff = Math.round(diff / 60); // minutes
+  if (diff < 60) return `${diff}m ago`;
+
+  diff = Math.round(diff / 60); // hours
+  if (diff < 24) return `${diff}h ago`;
+
+  diff = Math.round(diff / 24); // days
+  if (diff < 7) return `${diff} day${diff === 1 ? '' : 's'} ago`;
+
+  let options: Intl.DateTimeFormatOptions = {};
+
+  if (date.getFullYear() === now.getFullYear())
     options = { month: 'short', day: 'numeric' };
   else options = { year: 'numeric', month: 'numeric', day: 'numeric' };
 
