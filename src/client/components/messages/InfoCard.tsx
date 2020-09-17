a81093ff06bb025e6c4f76da04281db75d79c5c2
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-09-17 12:03:20 -0700

    Client: created InfoCard to store brief data of a conversation like recipient, last message, which listing it refers to, etc

---
+++ src/client/components/messages/InfoCard.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { ListingDoc, MessageDoc, UserDoc } from '../../../common';
+import { Avatar } from '../UserAvatar';
+import { getTimeAgoAndDateStr } from '../../utilities';
+
+interface InfoCardProps {
+  listing: ListingDoc;
+  recipient: UserDoc;
+  lastMsg: MessageDoc;
+  onClick(): void;
+}
+
+export const InfoCard = (props: InfoCardProps): JSX.Element => {
+  return (
+    <div className="messenger__info-cards__item" onClick={props.onClick}>
+      <Avatar user={props.recipient} useLink className="avatar--icon" />
+      <div className="messenger__info-cards__item__middle">
+        <p className="messenger__info-cards__item__recipient-name">
+          {props.recipient.name}
+        </p>
+        <p className="messenger__info-cards__item__last-msg">
+          {props.lastMsg.sender == props.recipient.id ? '' : 'You: '}
+          {props.lastMsg.content}
+        </p>
+        <p className="messenger__info-cards__item__last-sent-at">
+          {getTimeAgoAndDateStr(props.lastMsg.createdAt)}
+        </p>
+      </div>
+      <Link
+        className="messenger__info-cards__item__listing-photo"
+        to={`/listings/${props.listing.id}`}
+      >
+        <img src={`/img/listings/${props.listing.photos[0]}`} />
+      </Link>
+    </div>
+  );
+};
