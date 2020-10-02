d2f0779f80aebfd06a4c8a974263abc4aacced2c
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-10-02 16:45:17 -0700

    Server: use Sendgrid API to send welcome email using their dynamic template

--- src/client/components/About.tsx
+++ src/client/components/About.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { FaLinkedin, FaGithub } from 'react-icons/fa';
 import { RiMailSendLine } from 'react-icons/ri';
 
--- src/server/utils/email.ts
+++ src/server/utils/email.ts
@@ -1,22 +1,12 @@
 import nodemailer from 'nodemailer';
+import sgMail from '@sendgrid/mail';
 
-const transporter =
-  process.env.NODE_ENV === 'production'
-    ? nodemailer.createTransport({
-        service: 'SendGrid',
-        auth: {
-          user: process.env.SENDGRID_USERNAME,
-          pass: process.env.SENDGRID_PASSWORD,
-        },
-      })
-    : nodemailer.createTransport({
-        host: 'smtp.mailtrap.io',
-        port: 2525,
-        auth: {
-          user: process.env.MAILTRAP_USERNAME,
-          pass: process.env.MAILTRAP_PASSWORD,
-        },
-      });
+if (process.env.SENDGRID_API_KEY)
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const templates = {
+  welcome: 'd-7b877315ec5047e194466653fb16174f',
+};
 
 const send = async (
   email: string,
@@ -25,12 +15,22 @@ const send = async (
   html: string
 ): Promise<void> => {
   const mailOptions = {
-    from: `Ngan Nguyen <${process.env.EMAIL_FROM}>`,
+    from: `plistings <${process.env.EMAIL_FROM}>`,
     to: email,
     subject,
     text,
     html,
   };
+
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    auth: {
+      user: process.env.MAILTRAP_USERNAME,
+      pass: process.env.MAILTRAP_PASSWORD,
+    },
+  });
+
   await transporter.sendMail(mailOptions);
 };
 
@@ -53,7 +53,25 @@ export const sendWelcomeEmail = async (
   name: string,
   email: string
 ): Promise<void> => {
-  const text = `Hello ${name}, welcome to PListings! We're happy you're interested in PListings. Feel free to reach out to us at ${process.env.EMAIL_FROM} if you have any questions. In the mean time, post your items to sell and start earning some money.`;
-  const html = `<div><h3>Hi ${name}, welcome to PListings!</h3><p>We're happy you're interested in PListings. Feel free to reach out to us at ${process.env.EMAIL_FROM} if you have any questions.</p><p>In the mean time, post your items to sell and start earning some money.</p></div>`;
-  await send(email, 'Welcome to PListings!', text, html);
+  if (process.env.NODE_ENV === 'production') {
+    const msg = {
+      to: email,
+      from: process.env.EMAIL_FROM!,
+      templateId: templates.welcome,
+      dynamic_template_data: { name },
+    };
+
+    try {
+      await sgMail.send(msg);
+    } catch (err) {
+      console.log(err);
+      if (err.response) {
+        console.error(err.response.body);
+      }
+    }
+  } else {
+    const text = `Hello ${name}, welcome to PListings! We're happy you're interested in PListings. Feel free to reach out to us at ${process.env.EMAIL_FROM} if you have any questions. In the mean time, post your items to sell and start earning some money.`;
+    const html = `<div><h3>Hi ${name}, welcome to PListings!</h3><p>We're happy you're interested in PListings. Feel free to reach out to us at ${process.env.EMAIL_FROM} if you have any questions.</p><p>In the mean time, post your items to sell and start earning some money.</p></div>`;
+    await send(email, 'Welcome to PListings!', text, html);
+  }
 };
