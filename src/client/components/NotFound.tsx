68a9c0951a360664b12cda7fb22b8413cd62110a
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-10-02 10:56:41 -0700

    Client: styled PageNotFound

--- src/client/components/NotFound.tsx
+++ src/client/components/NotFound.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 export const NotFound = (): JSX.Element => {
   return (
-    <div>
-      <h1>Page Not Found</h1>
+    <div className="page-not-found">
+      <h1 className="page-not-found__header">404</h1>
+      <h2 className="page-not-found__subheader">Sorry, page not found</h2>
     </div>
   );
 };
